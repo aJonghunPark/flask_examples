e776eccda42795b0fbae2ac07e350c1ee9c9c030
Jonghun Park - 朴 鍾勲 <devtopia@icloud.com>
2023-02-19 20:34:34 +0900

    Add breed attribute to the puppy model

--- apps/section10/models.py
+++ apps/section10/models.py
@@ -9,10 +9,12 @@ class Puppy(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     age = db.Column(db.Integer)
+    breed = db.Column(db.String(120))
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, breed):
         self.name = name
         self.age = age
+        self.breed = breed
 
     def __repr__(self):
         return f"Puppy {self.name} is {self.age} years old"
