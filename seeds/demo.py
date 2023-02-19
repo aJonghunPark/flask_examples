ca1cb516e8cb801a7a12ec82cf86c8f6461e0ec6
Jonghun Park - 朴 鍾勲 <devtopia@icloud.com>
2023-02-19 22:13:38 +0900

    Add breed column to the seed

--- seeds/demo.py
+++ seeds/demo.py
@@ -1,5 +1,14 @@
+import random
+
 from apps.section10.models import Puppy
 from flask_seeder import Faker, Seeder, generator
+
+breeds = ("Golden Retrievers", "Boston Terriers", "Labrador Retrievers",
+          "Poodles", "Border Collie", "Beagle", "Irish Setter",
+          "Staffordshire Bull Terrier", "Cavalier King Charles Spaniel",
+          "Cockapoo", "Boxer", "Shih Tzu", "French Bulldog", "Basset Hound",
+          "Cocker Spaniel", "Greyhound", "Great Dane", "Samoyed",
+          "West Highland Terriers", "Pembroke Welsh Corgi")
 
 
 class PuppySeeder(Seeder):
@@ -9,8 +18,10 @@ class PuppySeeder(Seeder):
                       init={
                           "name": generator.Name(),
                           "age": generator.Integer(start=1, end=10),
+                          "breed": '',
                       })
 
         for puppy in faker.create(10):
             print(f"Adding puppy: {puppy}")
+            puppy.breed = random.choice(breeds)
             self.db.session.add(puppy)
