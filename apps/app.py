a3fd24b55550142c90ae2e401a7f7f0aceb682f8
Jonghun Park - 朴 鍾勲 <devtopia@icloud.com>
2023-02-22 11:47:38 +0900

    Disable using the common template

--- apps/app.py
+++ apps/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_seeder import FlaskSeeder
 from flask_sqlalchemy import SQLAlchemy
@@ -8,9 +9,10 @@ from apps.config import config
 
 db = SQLAlchemy()
 
+login_manager = LoginManager()
+
 
 def create_app(config_key):
-
     app = Flask(__name__)
     app.config.from_object(config[config_key])
 
@@ -20,20 +22,31 @@ def create_app(config_key):
     seeder = FlaskSeeder()
     seeder.init_app(app, db)
 
+    login_manager.init_app(app)
+    login_manager.login_view = "section12.login"
+
     from apps.section07.views import section07
+
     app.register_blueprint(section07, url_prefix="/section07")
 
     from apps.section08.views import section08
+
     app.register_blueprint(section08, url_prefix="/section08")
 
     from apps.section09.views import section09
+
     app.register_blueprint(section09, url_prefix="/section09")
 
     from apps.section10.views import section10
+
     app.register_blueprint(section10, url_prefix="/section10")
 
-    app.register_error_handler(404, page_not_found)
-    app.register_error_handler(500, internal_server_error)
+    from apps.section12.views import section12
+
+    app.register_blueprint(section12, url_prefix="/section12")
+
+    # app.register_error_handler(404, page_not_found)
+    # app.register_error_handler(500, internal_server_error)
 
     app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
     DebugToolbarExtension(app)
@@ -41,11 +54,11 @@ def create_app(config_key):
     return app
 
 
-def page_not_found(e):
-    """ 404 Not Found  """
-    return render_template("404.html"), 404
-
-
-def internal_server_error(e):
-    """ 500 Internal Server Error """
-    return render_template("500.html"), 500
+# def page_not_found(e):
+#     """ 404 Not Found  """
+#     return render_template("404.html"), 404
+#
+#
+# def internal_server_error(e):
+#     """ 500 Internal Server Error """
+#     return render_template("500.html"), 500
